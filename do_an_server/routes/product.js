c43e8366b3d0a45b26cef65a173d9c150356b65b
machchitai <65746537+machchitai@users.noreply.github.com>
2021-03-02 08:53:46 +0700

    tao route và lay data tu database fashionstore cua hai bang product va custome

---
+++ do_an_server/routes/product.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'fashionstore'
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM product', function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          res.json(results);
+        });
+    });
+
+});
+
+module.exports = router;
